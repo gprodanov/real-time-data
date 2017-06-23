61b29ba6108b109c560d0729e92a462a8ace9940
gngeorgiev <gngeorgiev.it@gmail.com>
2017-06-23 12:03:40 +0300

    efafad

--- real-time-broker-service/index.js
+++ real-time-broker-service/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var nsq = require('nsq.js');
+var nsq = require('nsqjs');
 
 const app = express();
 app.use(bodyParser.json());
 
-var writer = nsq.writer('192.168.130.50:4150');
+var writer = new nsq.Writer('192.168.130.50', 4150);
+
+writer.connect();
 
 writer.on('ready', () =>
     app.listen(8013, () => console.log('listening on 8013'))
